3ce211b1716d8bac047e75eed85da5b2769a5561
Jun Zhao <zhaojunwater@gmail.com>
2019-02-23 22:24:48 -0500

    nn

--- mlexp/ResultViewController.swift
+++ mlexp/ResultViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Vision
 
-class ResultViewController: BaseViewController {
+class ResultViewController: BaseViewController{
+    
+    let model = try? VNCoreMLModel(for: Inceptionv3().model)
+    
+    var image = UIImage()
+    
+    @IBOutlet weak var predictClass: UILabel!
+    @IBOutlet weak var imageUi: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imageUi.image = image
+        updateClassifications(for: image)
+        
+        /*
+        if classifications.isEmpty {
+            self.predictClass.text = "Nothing recognized."
+        } else {
+            guard let firstObservation = classifications.first else { return }
+            self.predictClass.text = "Classification: " + firstObservation.identifier
+        }*/
         
     }
     
@@ -23,4 +41,63 @@ class ResultViewController: BaseViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    
+    // 这个request包含两个主要的东西，一个是model，一个是执行model的方法processClassifications
+    /// - Tag: MLModelSetup
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            
+            let request = VNCoreMLRequest(model: model!, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    // 当picker dismiss之后，会执行这个func，它将UIImage转换成model用的CGImage，通过request handler执行classificationRequest
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        predictClass.text = "Classifying..."
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                print("Unable to classify image.")
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+            if classifications.isEmpty {
+                self.predictClass.text = "Nothing recognized."
+            } else {
+                guard let firstObservation = classifications.first else { return }
+                self.predictClass.text = "Classification: " + firstObservation.identifier
+            }
+        }
+    }
+    
 }
--- mlexp/ViewController.swift
+++ mlexp/ViewController.swift
@@ -7,80 +7,44 @@
 //
 
 import UIKit
-import AVKit
-import Vision
+import AVFoundation
 
-class ViewController: BaseViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-    let captureSession = AVCaptureSession()
-    let model = try? VNCoreMLModel(for: Inceptionv3().model)
+class ViewController: BaseViewController, AVCapturePhotoCaptureDelegate {
     
-    // IBOutlets
-    @IBOutlet weak var classLabel: UILabel! {
-        didSet {
-            classLabel.isExclusiveTouch = true
-            classLabel.font = UIFont.titleFont
-        }
-    }
+    
+    let captureSession = AVCaptureSession()
+    var output: AVCapturePhotoOutput?
+    var selectedImage = UIImage()
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.setupSession()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        
         super.viewDidAppear(animated)
         
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
     
+    /*
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }*/
     
-    func setupSession() {
-        self.view.layoutIfNeeded()
-        
-        captureSession.sessionPreset = .photo
-        
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        captureSession.addInput(input)
-        
-        let captureOutput = AVCaptureVideoDataOutput()
-        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        captureSession.addOutput(captureOutput)
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        previewLayer.frame = view.frame
-        previewLayer.borderColor = UIColor.black.cgColor
-        view.layer.insertSublayer(previewLayer, at: 0)
-        
-        captureSession.startRunning()
-    }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        //print("haha", Date())
-        //guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
-        
-        guard let model = self.model else { return }
-        
-        let request = VNCoreMLRequest(model: model){ (finishedReq, err) in
-            //perhaps check the err
-            //print(finishedReq.results)
-            
-            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-            guard let firstObservation = results.first else { return }
-            
-            //print(firstObservation.identifier, firstObservation.confidence)
-            DispatchQueue.main.async {
-                
-            }
-        }
-        
-        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
-    }
     
+    
+    
+    // 这个可以移到resultview里去
+    /*
     @IBAction func actionClassLabelTap(_ sender: UITapGestureRecognizer) {
         guard let label = sender.view as? UILabel, let classString = label.text else {
             return
@@ -95,6 +59,83 @@ class ViewController: BaseViewController, AVCaptureVideoDataOutputSampleBufferDe
         } else {
             UIApplication.shared.openURL(url)
         }
+    }*/
+    
+    // 这部分用来处理拍照并传递图片到result中去做prediction ====================================
+    @IBAction func takePhoto(_ sender: Any) {
+        
+        guard let capturePhotoOutput = self.output else { return }
+        
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        capturePhotoOutput.capturePhoto(with: settings, delegate: self)
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation()
+            else { return }
+        
+        selectedImage = UIImage(data: imageData)!
+        UIImageWriteToSavedPhotosAlbum(selectedImage, nil, nil, nil)
+        self.performSegue(withIdentifier: "baseToResultSegue", sender: self)
+    }
+    
+    func setupSession() {
+        self.view.layoutIfNeeded()
+        
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        output = AVCapturePhotoOutput()
+        
+        if captureSession.canAddInput(input) && captureSession.canAddOutput(output!) {
+            captureSession.addInput(input)
+            captureSession.addOutput(output!)
+        }
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = view.frame
+        previewLayer.borderColor = UIColor.black.cgColor
+        view.layer.insertSublayer(previewLayer, at: 0)
+        
+        captureSession.startRunning()
+    }
+    
+    // 这部分用来处理在相册里选择相片 ==========================================================
+    @IBAction func choosePhotos() {
+        self.presentPhotoPicker(sourceType: .photoLibrary)
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+    
+    // 因为计算predict需要较长的时间，所以我觉得当选择照片或拍了照片之后，将照片传到resultView再进行计算
+    // 这部分用来处理将predict的结果传递到ResultViewController ================================
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "baseToResultSegue"){
+            let resultVC = segue.destination as! ResultViewController
+            resultVC.image = selectedImage
+        }
+    }
+}
+
+// Jun ============
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: - Handling Image Picker Selection 当选择完照片后执行的操作。
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        
+        picker.dismiss(animated: true)
+        
+        selectedImage = (info[.originalImage] as? UIImage)!
+        self.performSegue(withIdentifier: "baseToResultSegue", sender: self)
     }
 }
 
